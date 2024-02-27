9aa63352d8f33a3ac58df4726dc02b9aebadc349
user <user@WIN>
2024-02-27 16:41:57 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace list
+{
+    internal class Program
+    {
+        //Перевірка чи список порожній
+        public static bool IsEmpty<T>(List<T> list) => list.Count == 0;
+        
+        public static void Main(string[] args)
+        {
+            var list = new List<int> { 1, 2, 3, 4, 5 };
+            Console.WriteLine(IsEmpty(list)); // False
+            list.Clear();
+            Console.WriteLine(IsEmpty(list)); // True
+        }
+    }
+}
